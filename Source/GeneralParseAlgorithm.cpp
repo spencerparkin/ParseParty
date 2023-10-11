1449d4c194b65c241039e4b244beca345ea72d48
Spencer T. Parkin <Spencer.Parkin@L3T.com>
2023-10-11 10:53:48 -0600

    Typeo.

--- Source/GeneralParseAlgorithm.cpp
+++ Source/GeneralParseAlgorithm.cpp
@@ -14,7 +14,7 @@ GeneralParseAlgorithm::GeneralParseAlgorithm(const std::vector<Lexer::Token*>* t
 {
 	// TODO: I have no idea how to write this.  Still thinking about it.
 
-	// TODO: It's hard to reserach in the literature on this subject, because it's super technical and I don't understand most of it, but
+	// TODO: It's hard to research in the literature on this subject, because it's super technical and I don't understand most of it, but
 	//       I wonder if the problem I'm trying to solve here is actually undecidable; meaning, no algorithm exists which could solve it
 	//       in a finite amount of time.  More than likely, I'm just not smart enough to figure it out.
 
